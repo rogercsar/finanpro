989d314bc6f4089ada45f84c2d9852f7be0d9710
Roger César <rogercsardev@gmail.com>
2025-11-30 01:35:02 -0400

    implementacao de funcionalidades novas 2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -92,7 +92,7 @@ export default function Layout() {
                     </div>
                 </div>
 
-                <nav className="flex-1 px-4 space-y-2">
+                <nav className="flex-1 px-4 space-y-2 overflow-y-auto">
                     {navItems.map((item) => {
                         const Icon = item.icon;
                         const isActive = location.pathname === item.path;
